dcceff48c37a69d52e77b103ef7738b062f68ea8
Gustavo Rodrigues <Gustavorr001@gmail.com>
2022-10-08 11:50:57 -0300

    subindo o projeto

---
+++ src/scripts/script.js
@@ -0,0 +1,13 @@
+const generateAdvice = async () => {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+
+    document.querySelector('.title').innerHTML = `Advice #${data.slip.id}`;
+    document.querySelector('.text').innerHTML = `"${data.slip.advice}"`;
+};
+
+document.querySelector('.btn').addEventListener('click', () => {
+    generateAdvice();
+});
+
+
